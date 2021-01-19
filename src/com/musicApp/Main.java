11b44282efbbc36e4168798408586cc1c21ecca6
Lau1259 <62459638+Lau1259@users.noreply.github.com>
2021-01-19 17:02:16 -0300

    Verified root change success!

--- src/com/musicApp/Main.java
+++ src/com/musicApp/Main.java
@@ -7,7 +7,7 @@ public class Main {
     anyways, I think we should each write our names and then re-upload
     this to GitHub.
     */
+        //Root change successful! 
         System.out.println("Lautaro Cuevas - Software Architect");
-        //Testing if my file change worked.
     }
 }
