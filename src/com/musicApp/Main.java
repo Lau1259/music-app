01241ec151c3efd3f1b892aca3815c8c41b084e0
Lau1259 <62459638+Lau1259@users.noreply.github.com>
2021-01-19 16:10:26 -0300

    Initial Push

---
+++ src/com/musicApp/Main.java
@@ -0,0 +1,12 @@
+package com.musicApp;
+
+public class Main {
+
+    public static void main(String[] args) {
+    /* Hey guys I thought we could use this to test out GitHub.
+    anyways, I think we should each write our names and then re-upload
+    this to GitHub.
+    */
+        System.out.println("Lautaro Cuevas - Software Architect");
+    }
+}
